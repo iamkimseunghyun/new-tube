36c3700e4713f237f7d966540aaf398bacb4896a
Kim Seung Hyun <kaka@Kimui-Macmini.local>
2025-02-27 16:53:59 +0900

    setup database neon & drizzle

---
+++ src/db/schema.ts
@@ -0,0 +1,16 @@
+import { uniqueIndex, uuid, timestamp, text } from 'drizzle-orm/pg-core';
+import { pgTable } from 'drizzle-orm/pg-core/table';
+
+export const users = pgTable(
+  'users',
+  {
+    id: uuid('id').primaryKey().defaultRandom(),
+    clerkId: text('clerkId').unique().notNull(),
+    name: text('name').notNull(),
+    // TODO: add banner fields
+    imageUrl: text('imageUrl').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+  },
+  (t) => [uniqueIndex('clerk_id_idx').on(t.clerkId)]
+);
