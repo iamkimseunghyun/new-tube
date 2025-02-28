669f892fe6a685ba198a408567777ebe5601e7a4
Kim Seung Hyun <kaka@Kimui-Macmini.local>
2025-02-28 15:00:54 +0900

    ngrok account setup, static domain for dev mode, concurrently run local tunnel and app, create users webhook in clerk, connect webhook on clerk dashboard

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  eslintConfigPrettier,
 ];
 
-export default [eslintConfig, eslintConfigPrettier];
+export default eslintConfig;
