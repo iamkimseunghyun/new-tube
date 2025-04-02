cb677fd56fccb13646931ea04cd7c2a453ed2ef1
Kim Seung Hyun <kaka@Kimui-Macmini.local>
2025-04-02 20:41:45 +0900

    06-trpc-setup-done

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,19 @@
-import Image from 'next/image';
+import { HydrateClient, trpc } from '@/trpc/server';
+import { PageClient } from '@/app/(home)/client';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
-const Home = () => {
-  return <div>I will load videos in the future!</div>;
+const Home = async () => {
+  void trpc.hello.prefetch({ text: 'kaka' });
+
+  return (
+    <HydrateClient>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ErrorBoundary fallback={<p>Error...</p>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
+  );
 };
 export default Home;
